2e5f2cbcd06eaf0c0c68ca777ab328afa0ae992a
Pingvu <nhlpool27@gmail.com>
2023-01-25 10:10:39 +0100

    Add project files.

---
+++ övning 2.3 variabler i cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace uppgift3
+{
+    class program
+    {
+         static void Main(string[]args)
+         {
+           Console.WriteLine("vad är ditt personliga bästa i längdhopp i centimeter");
+           string längdtext = Console.ReadLine();
+           float längd = float.Parse(längdtext);
+           double diff = 895 - längd;
+           Console.WriteLine("du hoppar " + diff + " centimeter kortare än världsrekordet");
+        }
+    }
+}
